2b4600040c01c70ecbcf075efa6a49f3b2902fc8
unknown <carlos.aguilar@outlook.com>
2023-06-04 09:23:27 -0600

    Suma de strings escuchando los eventos que presiona el usuario

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const pclass = document.querySelector('.pclass');//usando el querySelector para mandarlo llamar es con . (.pclass) como en css
-const pid = document.querySelector('#pid'); // y para un ID es usando el # (igual como en css)
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const resultP = document.getElementById('resultado');
 
+//usamos el "onclick" y el "onchange" para escuchar los eventos del usuario dentro de la página (onclick para escuchar el evento del botón y onchancge para el input)
+
+//vamos a crear una función para evitar tanta línea del código y así evitar hacer un evento por cada clic o cambio  que haga el  usuario 
+
+function btnOnClick(){
+    const sumaInputs = input1.value + input2.value;
+    resultP.innerHTML = sumaInputs;
+}
 
